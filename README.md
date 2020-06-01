c9f3bbf2ffaf7d84db07474da578d95ad929dc09
gonggt1991 <gonggt1991@163.com>
2020-06-01 23:31:00 +0800

    [BASE] Replace files.

--- README.md
+++
@@ -1,2 +0,0 @@
-# mediaplayer_dev_20200503
-This project aims to build a media player for playing local media files.
--- app/src/main/java/com/broccoli/musicplayer/bean/AccountBean.java
+++
@@ -1,7 +0,0 @@
-package com.broccoli.musicplayer.bean;
-
-public class AccountBean {
-    public AccountBean() {
-        super();
-    }
-}
